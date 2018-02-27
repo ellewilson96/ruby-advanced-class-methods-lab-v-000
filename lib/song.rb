03d81014db9c8e3b170230e03b72ff5143fd6cd0
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-27 20:50:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,10 +21,8 @@ class Song
     new_song
   end
 
-  def self.create_by_name(name)
-    new_song = self.new
-    new_song.name = name
-    new_song.save
-
+  def self.create_by_name
+    self.new_by_name
+    self.create
   end
 end
